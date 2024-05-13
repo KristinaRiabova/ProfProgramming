05de62e088fc038c405be9a560b6f0e14ddee38d
vovaskochko <50803350+vovaskochko@users.noreply.github.com>
2024-05-13 16:51:40 +0300

    Update LargestNegativeValueFinder.java

--- home1.1/LargestNegativeValueFinder.java
+++ home1.1/LargestNegativeValueFinder.java
@@ -12,10 +12,14 @@ public class LargestNegativeValueFinder {
         // Split the input into an array of strings
         String[] numbersAsString = input.split(" ");
 
+        // 1. There should be some conversion from string to integer and checking the correctness of the input
+        // 2. Also there should be a way to find, filter and find maximum value by built in tools already present in JDK
+        // 3. What if numbersAsString is empty?
         // Initialize variables to track the largest negative value and whether any negative value was found
         int largestNegative = numbersAsString[0];
         
-
+        // 4. The following loop looks strange: i is not initialized, we compare it with array of strings
+        // and we use index for comparison instead of array element
         for (int 0; i<numbersAsString; i++){
             if (i > largestNegative) {
                 largestNegative = i;
@@ -28,7 +32,11 @@ public class LargestNegativeValueFinder {
         } else {
             System.out.println("No negative values found in the list.");
         }
-
+        // 5. I hope that we can close scanner earlier just after we finished the real usage of it
         scanner.close();
+        // 6. As we are using Java and it is OOP language then it is better to encapsulate parts of logic:
+        // - one method for reading data and validation
+        // - another method to process data and calculate result
+        // - print can be done in main
     }
 }
